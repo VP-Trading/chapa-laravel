b5d015f30f9557adf674cd8b422b115ac355d872
Alazar Kassahun <alazar.kas@gmail.com>
2025-08-28 14:53:04 +0300

    removed unnecessary config variables

--- config/chapa.php
+++ config/chapa.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 return [
     'secret_key' => env('CHAPA_SECRET_KEY'),
-    'public_key' => env('CHAPA_PUBLIC_KEY'),
-    'encryption_key' => env('CHAPA_ENCRYPTION_KEY'),
     'base_url' => env('CHAPA_BASE_URL', 'https://api.chapa.co/v1'),
-    'callback_url' => env('CHAPA_CALLBACK_URL'),
+    'callback_url' => env('CHAPA_CALLBACK_URL', 'https://your-callback-url.com/vp/chapa/webhook'),
     'ref_prefix' => env('CHAPA_REF_PREFIX', 'vp_chapa_'),
-    'webhook_endpoint' => env('CHAPA_WEBHOOK_ENDPOINT', 'vp/chapa/webhook'),
 ];
