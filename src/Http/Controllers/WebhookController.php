b188dbb345f5f80b3233eab0095c757213de1ac8
Alazar Kassahun <alazar.kas@gmail.com>
2025-08-28 21:20:53 +0300

    added arch test and webhook secret check

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -11,6 +11,16 @@ class WebhookController
 {
     public function __invoke(Request $request)
     {
+        if (! empty(config('chapa.secret_key'))) {
+            $secret = config('chapa.webhook_secret');
+
+            $hash = hash_hmac('sha256', $request->getContent(), $secret);
+
+            if (! hash_equals($hash, $request->header('Chapa-Signature'))) {
+                return response()->json(['message' => 'Invalid signature'], 403);
+            }
+        }
+
         $content = json_decode($request->getContent(), true);
 
         ChapaWebhookEvent::create([
