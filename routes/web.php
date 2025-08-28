05591a28d336344ecc89583b85f33e721c60f7cf
Alazar Kassahun <alazar.kas@gmail.com>
2025-08-28 14:53:13 +0300

    added name to route

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Vptrading\ChapaLaravel\Http\Controllers\WebhookController;
 
-Route::post(config('chapa.webhook_endpoint'), WebhookController::class);
+Route::post(parse_url(config('chapa.callback_url'))['path'], WebhookController::class)->name('chapa.webhook');
