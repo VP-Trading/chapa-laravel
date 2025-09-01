a698065dec5f49515e03f69e24d24974bb44c7e8
Alazar Kassahun <alazar.kas@gmail.com>
2025-09-01 13:55:30 +0300

    updated doc block

--- src/Facades/Chapa.php
+++ src/Facades/Chapa.php
@@ -7,7 +7,7 @@ namespace Vptrading\ChapaLaravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static array acceptPayment(\Money\Money $amount, \Vptrading\ChapaLaravel\ValueObjects\UserValueObject $user, string $returnUrl)
+ * @method static array acceptPayment(\Money\Money $amount, \Vptrading\ChapaLaravel\ValueObjects\User $user, string $returnUrl)
  * @method static array verifyPayment(string $transactionId)
  */
 class Chapa extends Facade
