818c2078ae7e016412150686b9a137f51104fb19
Alazar Kassahun <alazar.kas@gmail.com>
2025-09-01 14:19:47 +0300

    updated facade definitions

--- src/Facades/Chapa.php
+++ src/Facades/Chapa.php
@@ -7,8 +7,9 @@ namespace Vptrading\ChapaLaravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static array acceptPayment(\Money\Money $amount, \Vptrading\ChapaLaravel\ValueObjects\User $user, string $returnUrl)
- * @method static array verifyPayment(string $transactionId)
+ * @method static \VpTrading\ChapaLaravel\Dtos\AcceptPaymentResponse acceptPayment(\Money\Money $amount, \Vptrading\ChapaLaravel\ValueObjects\User $user, string $returnUrl, \Vptrading\ChapaLaravel\ValueObjects\Customization|null $customization = null)
+ * @method static \VpTrading\ChapaLaravel\Dtos\VerifyPaymentResponse verifyPayment(string $transactionId)
+ * @method static \VpTrading\ChapaLaravel\Dtos\RefundResponse refund(string $transactionId, \Money\Money|null $amount = null, string|null $reason = null)
  */
 class Chapa extends Facade
 {
